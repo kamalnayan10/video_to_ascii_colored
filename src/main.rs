1358c67bd1be7d66e2fa685857495e8cbf0a4daf
kamalnayan10 <kamalnayan04@gmail.com>
2024-04-25 18:57:27 +0530

    added automatic fps detection , file cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::process::Command;
+use std::process::{Command,Stdio};
 use image::GenericImageView;
 use colored::Colorize;
 use crossterm::terminal;
-use std::{thread,time};
+use std::{fs,path::Path , time , thread};
 
 
 fn get_ascii(color_avg:u8) -> &'static str{
@@ -41,40 +41,87 @@ fn image_to_ascii(path:String){
     }
 }
 
-fn main() {
-    // Input video file
-    let input_file = "input.mp4";
 
-    // Output image files
-    let output_pattern = "frames/output_%04d.jpg";
+
+fn get_video_frame_rate(file_path: &str) -> Option<f32> {
+    let output = Command::new("ffprobe")
+        .args(&[
+            "-v",
+            "error",
+            "-select_streams",
+            "v:0",
+            "-show_entries",
+            "stream=r_frame_rate",
+            "-of",
+            "default=noprint_wrappers=1:nokey=1",
+            file_path,
+        ])
+        .stdout(Stdio::piped())
+        .output()
+        .expect("Failed to execute ffprobe command");
+
+    if let Ok(output_text) = String::from_utf8(output.stdout) {
+        let frame_rate_parts: Vec<&str> = output_text.trim().split('/').collect();
+        if frame_rate_parts.len() == 2 {
+            if let (Ok(num), Ok(den)) = (frame_rate_parts[0].parse::<f32>(), frame_rate_parts[1].parse::<f32>()) {
+                return Some(num / den);
+            }
+        }
+    }
+    None
+}
+
+
+fn main() {
+    let input_file = "anime.mp4";
+
+    let output_pattern = "frames/output_%d.jpg";
+
+    let mut fps: f32 = 1.00;
+
+    if let Some(frame_rate) = get_video_frame_rate(input_file){
+        fps = frame_rate;
+    }
+
 
     // Construct ffmpeg command to extract frames as JPEG images
     let mut ffmpeg_command = Command::new("ffmpeg");
     ffmpeg_command
         .arg("-i")
         .arg(input_file)
-        .args(&["-vf", "fps=1"])
+        .args(&["-vf", &format!("fps={fps}")])
         .arg("-q:v")
         .arg("2")
         .arg(output_pattern);
-
+    
+    
     // Execute the ffmpeg command
     let output = ffmpeg_command.output().expect("Failed to execute command");
 
-    // Check if the command was successful
     if output.status.success() {
         println!("Conversion successful!");
-        for i in 1..12{
-            let mut to_add = format!("0{i}");
-            if i >= 10{
-                to_add = String::from(format!("{i}"));
+           
+        let frames_directory = Path::new("frames");
+        let mut frame_count = 1;
+        if let Ok(entries) = fs::read_dir(&frames_directory) {
+        for entry in entries.flatten() {
+            if entry.file_type().map_or(false, |ft| ft.is_file()) {
+                
+                frame_count += 1; 
             }
-            image_to_ascii(format!("frames/output_00{to_add}.jpg"));
-            let ten_millis = time::Duration::from_millis(1000);
-            thread::sleep(ten_millis);
-            std::process::Command::new("clear").status().unwrap();
-        
         }
+
+        for i in 1..frame_count{
+            image_to_ascii(format!("frames/output_{i}.jpg"));
+            let sleep_time = time::Duration::from_millis(fps as u64);
+            thread::sleep(sleep_time);
+            let _ = fs::remove_file(format!("frames/output_{i}.jpg"));
+              
+        } 
+        std::process::Command::new("clear").status().unwrap();  
+        
+        
+    }
 
     } else {
         println!(
